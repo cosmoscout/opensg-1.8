06e3e4d5cb8a6c6025ea18816c9455fb87e54bb0
Jonas Gilg <jonas.gilg@dlr.de>
2026-07-31 13:35:15 +0200

    :tada: Add export config and targets.

--- port/portfile.cmake
+++ port/portfile.cmake
@@ -1,19 +1,26 @@
-vcpkg_from_gitlab(
-    GITLAB_URL https://gitlab.com/OpenSG/OpenSG
-    REF v1.8.0
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO cosmoscout/opensg-1.8
+    REF 0
     SHA512 0
     HEAD_REF main
 )
 
+set(OPENSG_WINDOW_FEATURE Off)
+if ("window" IN_LIST FEATURES)
+    set(OPENSG_WINDOW_FEATURE On)
+endif ()
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DOPENSG_BUILD_TESTS=OFF
-        -DOPENSG_BUILD_WINDOW=$<$<IN_LIST:window,FEATURES>:ON>
+        -DOPENSG_BUILD_TESTS=Off
+        -DOPENSG_BUILD_WINDOW=${OPENSG_WINDOW_FEATURE}
+        -DOPENSG_USE_PRECOMPILED_HEADERS=On
+        -DOPENSG_INFINITE_REVERSE_PROJECTION=On
+        -DCMAKE_UNITY_BUILD=On
 )
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-
-file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME "copyright")
-file(INSTALL "${CURRENT_PORT_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
