6a766173985b91b43767b7a4e039726907ff0207
Jonas Gilg <jonas.gilg@dlr.de>
2026-08-03 11:35:55 +0200

    :wrench: Fix portfile warnings.

--- port/portfile.cmake
+++ port/portfile.cmake
@@ -22,5 +22,9 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME "OpenSG")
 vcpkg_copy_pdbs()
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
