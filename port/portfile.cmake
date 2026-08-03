df7aed8049c776d7478d240a575c6214d1c9f69f
Jonas Gilg <jonas.gilg@dlr.de>
2026-08-03 13:46:46 +0200

    :wrench: Remove warning from vcpkg port file config.

--- port/portfile.cmake
+++ port/portfile.cmake
@@ -26,4 +26,5 @@ vcpkg_copy_pdbs()
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
